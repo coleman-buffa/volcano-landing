0b2a01c6a8e5630b98b62bb670bbbc9ec22ec915
Coleman Buffa <coleman.buffa@gmail.com>
2021-04-30 18:19:45 -0700

    Draft header component and apply styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import {CssBaseline} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '100vh',
     backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/bg.jpg"})`,
-    backgroundRepeat:'no-repeat',
+    backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover'
   }
 }));
 
 export default function App() {
   const classes = useStyles();
-  return <div className = {classes.root}>
-    <CssBaseline/>
+  return <div className={classes.root}>
+    <CssBaseline />
+    <Header />
   </div>
 }
